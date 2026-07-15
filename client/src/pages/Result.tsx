2e3ca11b4551f4353845df68fae0ab2f4f371966
devmetilda <sequierametilda@gmail.com>
2026-07-15 15:20:38 +0530

    Added sidebarActions in result.tsx

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Loader2Icon, RefreshCwIcon } from "lucide-react";
+import {
+  Loader2Icon,
+  RefreshCwIcon,
+  ImageIcon,
+  VideoIcon,
+} from "lucide-react";
 import { Link } from "react-router-dom";
 
 import type { Project } from "../types";
 import { dummyGenerations } from "../assets/assets";
-
+import { GhostButton } from "../components/Buttons";
 
 const Result = () => {
   const [project, setProjectData] = useState<Project>({} as Project);
@@ -28,38 +33,86 @@ const Result = () => {
     </div>
   ) : (
     <div className="min-h-screen text-white p-6 md:p-12 mt-20">
-    <div className="max-w-6xl mx-auto">
-      <header className="flex justify-between items-center mb-8">
-        <h1 className="text-2xl md:text-3xl  font-medium">Generation Result</h1>
-        <Link to="/generate" className="btn-secondary text-sm flex items-center gap-2">
-        <RefreshCwIcon className="w-4 h-4" />
-        <p className="max-sm:hidden">New Generation</p>
-        </Link>
-              </header>
+      <div className="max-w-6xl mx-auto">
+        <header className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl md:text-3xl font-medium">
+            Generation Result
+          </h1>
+
+          <Link
+            to="/generate"
+            className="btn-secondary text-sm flex items-center gap-2"
+          >
+            <RefreshCwIcon className="w-4 h-4" />
+            <p className="max-sm:hidden">New Generation</p>
+          </Link>
+        </header>
 
         {/* grid layout */}
         <div className="grid lg:grid-cols-3 gap-8">
-          {/*Main Result Display */}
+          {/* Main Result Display */}
           <div className="glass-panel inline-block p-2 rounded-2xl">
-            <div className={ `${project?.aspectRatio === '9:16' ? 'aspect-9/16' : 
-            'aspect-video' } sm:max-h-200 rounded-xl bg-gray-900 overflow-hidden relative`}>
+            <div
+              className={`${
+                project?.aspectRatio === "9:16"
+                  ? "aspect-[9/16]"
+                  : "aspect-video"
+              } sm:max-h-[800px] rounded-xl bg-gray-900 overflow-hidden relative`}
+            >
               {project?.generatedVideo ? (
-                <video src={project.generatedVideo} controls autoPlay loop className="w-full h-full object-cover"/>
+                <video
+                  src={project.generatedVideo}
+                  controls
+                  autoPlay
+                  loop
+                  className="w-full h-full object-cover"
+                />
               ) : (
-                <img src={project.generatedImage} alt="Generated Result" className="w-full h-full object-cover"/>            
+                <img
+                  src={project.generatedImage}
+                  alt="Generated Result"
+                  className="w-full h-full object-cover"
+                />
               )}
             </div>
-
           </div>
 
           {/* Sidebar Actions */}
-          <div></div>
-        </div>
+          <div className="space-y-6">
+            {/* download buttons */}
+            <div className="glass-panel p-6 rounded-2xl">
+              <h3 className="text-xl font-semibold mb-4">Actions</h3>
 
+              <div className="flex flex-col gap-3">
+                <a href={project.generatedImage || "#"} download>
+                  <GhostButton
+                    disabled={!project.generatedImage}
+                    className="w-full justify-center rounded-md py-3 disabled:opacity-50 disabled:cursor-not-allowed"
+                  >
+                    <ImageIcon className="size-4.5" />
+                    Download Image
+                  </GhostButton>
+                </a>
+
+                <a href={project.generatedVideo || "#"} download>
+                  <GhostButton
+                    disabled={!project.generatedVideo}
+                    className="w-full justify-center rounded-md py-3 disabled:opacity-50 disabled:cursor-not-allowed"
+                  >
+                    <VideoIcon className="size-4.5" />
+                    Download Video
+                  </GhostButton>
+                </a>
+              </div>
+            </div>
+
+            {/* generate video button */}
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
   );
 };
-
 
 export default Result;
