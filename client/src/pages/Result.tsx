fea1689b36f936e1e105ec894afeeb74ac419692
devmetilda <sequierametilda@gmail.com>
2026-07-15 14:18:20 +0530

    Added Result.tsx and updated

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,9 +1,33 @@
-const Result = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import { Loader2Icon } from "lucide-react";
+import type { Project } from "../types";
+import { dummyGenerations } from "../assets/assets";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default Result
+const Result = () => {
+  const [project, setProjectData] = useState<Project>({} as Project);
+  const [loading, setLoading] = useState(true);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const fetchProjectData = async () => {
+    setTimeout(() => {
+      setProjectData(dummyGenerations[0]);
+      setLoading(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    fetchProjectData();
+  }, []);
+
+  return loading ? (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2Icon className="size-9 animate-spin text-indigo-500" />
+    </div>
+  ) : (
+    <div>
+    </div>
+  );
+};
+
+export default Result;
