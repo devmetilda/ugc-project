26390edd54f804353f8f5acdf32c46a300fea7eb
devmetilda <sequierametilda@gmail.com>
2026-07-15 15:59:25 +0530

    Completed frontend

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,9 +1,20 @@
-const Loading = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { Loader2Icon } from "lucide-react";
+import { useEffect } from "react";
 
-export default Loading
+const Loading = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 6000);
+  }, []);
+
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex items-center justify-center flex-1">
+        <Loader2Icon className="size-7 animate-spin text-indigo-200" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
