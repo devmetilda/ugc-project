408217692aec69aafcd66d3da45fb7120ef6fe0e
devmetilda <sequierametilda@gmail.com>
2026-07-16 10:48:20 +0530

    User athentication setup done by clerk

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,30 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'        
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { dark } from '@clerk/themes';
 
-createRoot(document.getElementById('root')! as HTMLElement).render(
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-)
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+createRoot(document.getElementById('root')!).render(
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    appearance={{
+      theme: dark,
+      variables: {
+        colorPrimary: '#4f39f6',
+        colorTextOnPrimaryBackground: '#ffffff',
+      },
+    }}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
+);
