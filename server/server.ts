131e9eb0164da72c0e6675dbd7d3bce485c5f995
devmetilda <sequierametilda@gmail.com>
2026-07-21 16:22:30 +0530

    Added and updated the middleware

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 //Middleware
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
