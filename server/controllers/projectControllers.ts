b59ac8f64a5f164a093dbd418018c35d16e92eb2
devmetilda <sequierametilda@gmail.com>
2026-08-08 12:05:01 +0530

    Added controllers functons to projectController.ts

---
+++ server/controllers/projectControllers.ts
@@ -0,0 +1,43 @@
+import {Request, Response } from 'express'
+import * as Sentry from '@sentry/node';
+
+
+export const createProject = async (req:Request, res:Response) => {
+    try{
+
+    }
+    catch(error:any){
+        Sentry.captureException(error);
+        res.status(500).json({ message: error.code || error.message });
+    }
+}
+
+export const createVideo = async (req:Request, res:Response) => {
+    try{
+        
+    }
+    catch(error:any){
+        Sentry.captureException(error);
+        res.status(500).json({ message: error.code || error.message });
+    }
+}
+
+export const getAllPublishedProjects = async (req:Request, res:Response) => {
+    try{
+        
+    }
+    catch(error:any){
+        Sentry.captureException(error);
+        res.status(500).json({ message: error.code || error.message });
+    }
+}
+
+export const deleteProject = async (req:Request, res:Response) => {
+    try{
+        
+    }
+    catch(error:any){
+        Sentry.captureException(error);
+        res.status(500).json({ message: error.code || error.message });
+    }
+}
