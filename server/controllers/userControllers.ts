0da897d9e1dbfa495a1fb0a52c8533cc64136883
devmetilda <sequierametilda@gmail.com>
2026-08-08 11:59:52 +0530

    Completed the logic for user controllers

--- server/controllers/userControllers.ts
+++ server/controllers/userControllers.ts
@@ -1,46 +1,90 @@
-import {Request, Response} from 'express'
-import * as Sentry from "@sentry/node"
+import { Request, Response } from 'express';
+import * as Sentry from "@sentry/node";
+import { prisma } from '../configs/prisma';
 
 // Get User Credits
-export const getUserCredits = async (req: Request, res:Response)=> {
+export const getUserCredits = async (req: Request, res: Response) => {
     try {
-        
-    } catch (error: any){
+        const { userId } = req.auth();
+        if (!userId) { return res.status(401).json({ message: 'Unauthorized' }); }
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId }
+        });
+        res.json({ credits: user?.credits });
+
+    } catch (error: any) {
         Sentry.captureException(error);
-        res.status(500).json({message: error.code || error.message})
+        res.status(500).json({ message: error.code || error.message });
     }
+};
 
-}
-
-// const get all user projects
-export const getAllProjects = async (req: Request, res:Response)=> {
+// Get all user projects
+export const getAllProjects = async (req: Request, res: Response) => {
     try {
+        const { userId } = req.auth();
+        const projects = await prisma.project.findMany({
+            where: { userId },
+            orderBy: { createdAt: 'desc' }
+        });
+        res.json({ projects });
 
-    } catch (error: any){
+    } catch (error: any) {
         Sentry.captureException(error);
-        res.status(500).json({message: error.code || error.message})
+        res.status(500).json({ message: error.code || error.message });
     }
+};
 
-}
-
-// get project by id
-export const getProjectById = async (req: Request, res:Response)=> {
+// Get project by id
+export const getProjectById = async (req: Request, res: Response) => {
     try {
+        const { userId } = req.auth();
+        const { projectId } = req.params;
 
-    } catch (error: any){
+        const project = await prisma.project.findFirst({
+            where: {
+                id: projectId as string,
+                userId: userId,
+            },
+        });
+
+        if (!project) { return res.status(404).json({ message: 'Project not found' }); }
+        res.json({ project });
+
+    } catch (error: any) {
         Sentry.captureException(error);
-        res.status(500).json({message: error.code || error.message})
+        res.status(500).json({ message: error.code || error.message });
     }
+};
 
-}
-
-// publish / unpublish project
-export const toggleProjectPublic = async (req: Request, res:Response)=> {
+// Publish / unpublish project
+export const toggleProjectPublic = async (req: Request, res: Response) => {
     try {
+        const { userId } = req.auth();
+        const { projectId } = req.params;
 
-    } catch (error: any){
+        const project = await prisma.project.findFirst({
+            where: {
+                id: projectId as string,
+                userId: userId,
+            },
+        });
+
+        if (!project) { return res.status(404).json({ message: 'Project not found' }); }
+
+        if (!project?.generatedImage && !project?.generatedVideo) {
+            return res.status(400).json({ message: 'Image or video not generated' });
+        }
+
+        const updatedProject = await prisma.project.update({
+    where: { id: projectId as string },
+    data: { isPublished: !project.isPublished }
+});
+
+        res.json({ isPublished: updatedProject.isPublished });
+
+    } catch (error: any) {
         Sentry.captureException(error);
-        res.status(500).json({message: error.code || error.message})
+        res.status(500).json({ message: error.code || error.message });
     }
-
-}
+};
